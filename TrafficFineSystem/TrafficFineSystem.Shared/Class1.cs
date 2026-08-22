4cf497dfb9e78bef82c491daca2b4c08efab1bbf
Emre Taştekin <emretastekin2369@gmail.com>
2026-08-22 16:03:00 +0300

    TrafficFineSystem
    Developed with .NET

---
+++ TrafficFineSystem/TrafficFineSystem.Shared/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace TrafficFineSystem.Shared;
+
+public class Class1
+{
+}
---
+++ TrafficFineSystem/TrafficFineSystem.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TrafficFineSystem.WebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
